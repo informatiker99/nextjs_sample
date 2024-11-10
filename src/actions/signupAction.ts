1fe7470357fa17490a4fcbec8f921ae97fa6d441
hossein seyedi <hosseincybercoin@gmail.com>
2024-11-10 23:23:48 +0330

    add athentication

--- src/actions/signupAction.ts
+++ src/actions/signupAction.ts
@@ -1,25 +1,50 @@
 "use server";
 
+import { createAuthSession } from "@/lib/auth";
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
 import { redirect } from "next/navigation";
-
 
 type FormActionParameters = {
   userName: string;
   password: string;
 };
 
-export const signup = async (state: FormActionParameters, data: FormData) => {
-  const userName = data.get("userName") as string;
-  const password = data.get("password") as string;
-  const validatedInputs = {
-    userName: userName.trim(),
-    password: password.trim(),
-  };
-  if (!validatedInputs.password || !validatedInputs.userName) {
-    return {
-      errors: "all the field must be filled out ",
-    };
+export const signup = async (state, formData: FormData) => {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+  console.log(email,'=******');
+  
+  // if (!email.includes("@")) {
+  //   errors.email = "Please enter a valid email address.";
+  // }
+
+  // if (password.trim().length < 8) {
+  //   errors.password = "Password must be at least 8 characters long.";
+  // }
+
+  // if (Object.keys(errors).length > 0) {
+  //   return {
+  //     errors,
+  //   };
+  // } 
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/articles");
+
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "It seems like an account for the chosen email already exists.",
+        },
+      };
+    }
+    throw error;
   }
-  console.log(validatedInputs);
-  redirect('/articles')
 };
---
+++ src/lib/auth.ts
@@ -0,0 +1,72 @@
+import { Lucia } from "lucia";
+import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
+import db from "./db";
+import { cookies } from "next/headers";
+
+const adapter = new BetterSqlite3Adapter(db, {
+  user: "users",
+  session: "sessions",
+});
+
+const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
+
+export const createAuthSession = async (userId: any) => {
+  const session = await lucia.createSession(userId, {});
+
+  const sesssionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sesssionCookie.name,
+    sesssionCookie.value,
+    sesssionCookie.attributes
+  );
+};
+
+//   VERIFY AUTHENTICATED USERS
+
+export const verifyAuth = async () => {
+  const sessionCookie = cookies().get(lucia.sessionCookieName);
+  if (!sessionCookie) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const sessionId = sessionCookie.value;
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const result = await lucia.validateSession(sessionId);
+
+  try {
+    if (result.session && result.session.fresh) {
+      const sessioncookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch (error) {}
+
+  return result;
+};
